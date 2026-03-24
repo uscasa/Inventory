dd65e970867d35491e5f305eb670789593c9d407
uscasa <bilal@jakfulfillment.com>
2026-03-23 23:13:42 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'invctr-v1';
+const ASSETS = [
+  '/Inventory/',
+  '/Inventory/index.html',
+  '/Inventory/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
